0730d534b5c2e6a68039f77a1599d08400441d9f
Dh H <hdhye89@gmail.com>
2023-11-25 19:53:30 +0900

    secret remove

--- src/APIS/CardApi.js
+++ src/APIS/CardApi.js
@@ -15,7 +15,7 @@ export const CardInfoAPI = createAsyncThunk(
           // 'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
           // 'Access-Control-Allow-Headers': 'Content-Type',
           // 'Accept':'*/*',
-          'Authorization': `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdWQiOiIxMTAxMDQxMDg1Iiwic2NvcGUiOlsiY2FyZGluZm8iLCJsb2dpbiJdLCJpc3MiOiJodHRwczovL3d3dy5vcGVuYmFua2luZy5vci5rciIsImV4cCI6MTcwODU5Mzk0NiwianRpIjoiYTY0OTQzOTgtOWZmYy00OGFmLThlNWItN2QzNDI5ZTAzNzMyIn0.ZYdNTJSEJRWvodUY15HYDixtRM7TX-6duE_q5VbjqU4`,
+          'Authorization': process.env.REACT_APP_OB_AccessTest,
           
         },
         params: {
@@ -35,27 +35,4 @@ export const CardInfoAPI = createAsyncThunk(
     }
   }
 );
-
-// import axios from 'axios';
-
-// export const CardInfoAPI = (obData) => {
-//   console.log("data");
-
-//   const requestURL = `${process.env.REACT_APP_OB_CARDINFO_URL}?bank_tran_id=F120430304343&user_seq_no=1101041085&bank_code_std=399&member_bank_code=399`;
-
-//   return async (dispatch, state) => {
-//     try {
-//       const response = await axios.get(requestURL, {
-//         headers: {
-//           Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdWQiOiIxMTAxMDQxMDg1Iiwic2NvcGUiOlsiY2FyZGluZm8iLCJsb2dpbiJdLCJpc3MiOiJodHRwczovL3d3dy5vcGVuYmFua2luZy5vci5rciIsImV4cCI6MTcwODU5Mzk0NiwianRpIjoiYTY0OTQzOTgtOWZmYy00OGFmLThlNWItN2QzNDI5ZTAzNzMyIn0.ZYdNTJSEJRWvodUY15HYDixtRM7TX-6duE_q5VbjqU4`,
-//         },
-//       });
-
-//       console.log(response.data);
-//       return response.data;
-//     } catch (error) {
-//       console.error(error.message);
-//     }
-//   };
-// };
 
